e4ef08c592c828e49425a2fd31cca79ee8e29167
Sarah Shi <sarah.c.shi@gmail.com>
2023-03-22 00:15:15 -0400

    testing

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // Responsive navigation menu
-// const menuToggle = document.getElementById("menu-toggle");
-// const nav = document.querySelector("nav");
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav");
 
-// menuToggle.addEventListener("click", () => {
-//   nav.classList.toggle("open");
-// });
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
 
 // Smooth scrolling
 const navLinks = document.querySelectorAll(".nav-link");
