317c47f4bd37de9be683bd058faee0433a6632e1
Sarah Shi <sarah.c.shi@gmail.com>
2023-03-21 23:09:48 -0400

    recreate cargo.site format

--- scripts.js
+++ scripts.js
@@ -26,41 +26,62 @@ navLinks.forEach((link) => {
   });
 });
 
+// Get the height of the first image
+const imgHeight = document.querySelector('.column img').clientHeight;
+
+// Set the height of all images to be the same as the height of the first image
+const imgs = document.querySelectorAll('.column img');
+imgs.forEach(img => {
+  img.style.height = `${imgHeight}px`;
+});
+
+
+
+
 // Create a scrollingText function
-// function scrollingText() {
-//   const text = "This is a scrolling text example. ";
-//   const container = document.createElement("div");
-//   container.className = "scrolling-container";
-//   container.style.overflow = "hidden";
-//   container.style.position = "fixed";
-//   container.style.whiteSpace = "nowrap";
-//   container.style.top = "20px";
-//   container.style.left = "100%";
-//   container.style.fontSize = "24px";
-//   container.innerText = text;
+function scrollingText(element) {
+  const container = document.createElement("div");
+  container.className = "scrolling-container";
+  container.style.overflow = "hidden";
+  container.style.position = "fixed";
+  container.style.whiteSpace = "nowrap";
+  container.style.top = element.offsetTop + "px";
+  container.style.left = "100%";
+  container.style.fontSize = "24px";
+  container.appendChild(element);
 
-//   document.body.appendChild(container);
+  document.body.appendChild(container);
 
-//   // Define scroll speed
-//   const speed = 2;
+  // Define scroll speed
+  const speed = 2;
 
-//   // Animate the scrolling text
-//   function animate() {
-//     const containerWidth = container.getBoundingClientRect().width;
-//     const currentPosition = parseFloat(container.style.left);
-//     const newPosition = currentPosition - speed;
+  // Animate the scrolling text
+  function animate() {
+    const containerWidth = container.getBoundingClientRect().width;
+    const currentPosition = parseFloat(container.style.left);
+    const newPosition = currentPosition - speed;
 
-//     if (newPosition + containerWidth < 0) {
-//       container.style.left = "100%";
-//     } else {
-//       container.style.left = newPosition + "px";
-//     }
+    if (newPosition + containerWidth < 0) {
+      container.style.left = "100%";
+    } else {
+      container.style.left = newPosition + "px";
+    }
 
-//     requestAnimationFrame(animate);
-//   }
+    requestAnimationFrame(animate);
+  }
 
-//   requestAnimationFrame(animate);
-// }
+  requestAnimationFrame(animate);
+}
 
-// // Initialize scrollingText function when the page loads
-// window.addEventListener("load", scrollingText);
+// Initialize scrollingText function when the page loads
+window.addEventListener("load", function () {
+  const researchHeader = document.querySelector("#research h2");
+  const codeHeader = document.querySelector("#code h2");
+  const teachingHeader = document.querySelector("#teaching h2");
+  const fieldHeader = document.querySelector("#field h2");
+  
+  scrollingText(researchHeader);
+  scrollingText(codeHeader);
+  scrollingText(teachingHeader);
+  scrollingText(fieldHeader);
+});
